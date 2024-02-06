00b1548efc236e19d5fd8dcabac2c64ae0c7a3c8
CollinJenkins2803 <cpj00003@mail.wvu.edu>
2024-02-06 16:23:33 -0500

    Removed #go-to-top button as it all the sudden was not showing up. Don't have the time to fix, I will add it later on in dev.

--- Assingment_2_MIST-353-001/wwwroot/js/site.js
+++ Assingment_2_MIST-353-001/wwwroot/js/site.js
@@ -15,18 +15,6 @@ navToggleBtn.addEventListener("click", function () {
 
 
 /**
- * show go top btn when scroll window to 500px
- */
-
-const goTopBtn = document.querySelector("[data-go-top]");
-
-window.addEventListener("scroll", function () {
-    window.scrollY >= 500 ? goTopBtn.classList.add("active")
-        : goTopBtn.classList.remove("active");
-});
-
-
-/**
  * search bar js
  */
 function getLocation() {
