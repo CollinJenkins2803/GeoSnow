7e256b2830603c7f5acee791248ac912d30ab41c
CollinJenkins2803 <cjenks0303@gmail.com>
2024-03-22 01:37:52 -0400

    fixed partially

---
+++ Assingment_2_MIST-353-001/Pages/ContactUs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Assingment_2_MIST_353_001.Pages
+{
+    public class ContactUsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
